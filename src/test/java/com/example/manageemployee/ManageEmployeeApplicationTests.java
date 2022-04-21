f86d5a8c6c54a2736d8cd74c3d8df7a2d0b0bfe3
xuan.tranthithanh <xuan.tranthithanh@thalesdigital.io>
2022-04-21 23:47:41 +0200

    Initial commit

---
+++ src/test/java/com/example/manageemployee/ManageEmployeeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.manageemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageEmployeeApplicationTests {
+  
+  @Test void contextLoads() {
+  }
+  
+}
